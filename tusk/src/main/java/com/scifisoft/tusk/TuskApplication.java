09e5711b2a215334dab8398f3e66081be21b210f
ponkaj sarker <pongkaz89@gmail.com>
2019-04-02 22:00:16 +0600

    i cmmmit my project

---
+++ tusk/src/main/java/com/scifisoft/tusk/TuskApplication.java
@@ -0,0 +1,13 @@
+package com.scifisoft.tusk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuskApplication.class, args);
+	}
+
+}
---
+++ tusk/src/test/java/com/scifisoft/tusk/TuskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scifisoft.tusk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TuskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
